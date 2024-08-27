45f144b2ce5309e4456719826a79330e0bf3e3b2
Kevin Leister <kevinleister07@gmail.com>
2024-08-27 18:11:13 -0500

    add to cart from details view and refactor cart service

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -17,20 +17,17 @@ export class CartService {
     let alreadyExistsInCart: boolean = false;
 
     // @ts-ignore - ignore the strict type checking
+    let existingCartItem: CartItem = undefined;
     // could also comment the following in tsconfig.json:
     // "strict": true,
-    let existingCartItem: CartItem = undefined;
     // or use the following:
     // let existingCartItem: CartItem = new CartItem("", "", "", 0);
 
     if (this.cartItems.length > 0) {
       // find the item in the cart based on item id
-      for (let tempCartItem of this.cartItems) {
-        if (tempCartItem.id === theCartItem.id) {
-          existingCartItem = tempCartItem;
-          break;
-        }
-      }
+      existingCartItem = this.cartItems.find(
+        (tempCartItem) => tempCartItem.id === theCartItem.id
+      )!;
 
       // check if we found it
       alreadyExistsInCart = existingCartItem != undefined;
