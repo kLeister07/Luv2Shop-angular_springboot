63e8c748bf52ae10d727be857fe285ed41caf2fb
Kevin Leister <kevinleister07@gmail.com>
2024-08-27 14:28:58 -0500

    create model class: CartItem

---
+++ 03-frontend/angular-ecommerce/src/app/common/cart-item.ts
@@ -0,0 +1,29 @@
+import { Product } from './product';
+
+export class CartItem {
+
+    constructor(public id: string,
+                public name:string,
+                public imageUrl: string,
+                public unitPrice: number,
+                public quantity: number = 1) {
+    }
+}
+// export class CartItem {
+//   id: string;
+//   name: string;
+//   imageUrl: string;
+//   unitPrice: number;
+
+//   quantity: number;
+
+//   constructor(id: string, name: string, imageUrl: string, unitPrice: number) {
+//     this.id = id;
+//     this.name = name;
+//     this.imageUrl = imageUrl;
+//     this.unitPrice = unitPrice;
+
+//     this.quantity = 1;
+//   }
+// }
+
