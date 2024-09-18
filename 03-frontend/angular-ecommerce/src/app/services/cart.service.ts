2d3f5cd0594aa92023a92587a558e11218c3d3a9
Kevin Leister <kevinleister07@gmail.com>
2024-09-17 20:01:56 -0500

    modify computeCartTotals() to call new method persistCartItems(), saves in local storage

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -11,7 +11,8 @@ export class CartService {
   totalPrice: Subject<number> = new BehaviorSubject<number>(0);
   totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-  storage: Storage = sessionStorage;
+  // storage: Storage = sessionStorage;
+  storage: Storage = localStorage;
 
   constructor() {
     // read data from storage
@@ -70,6 +71,9 @@ export class CartService {
 
     // log cart data just for debugging purposes
     this.logCartData(totalPriceValue, totalQuantityValue);
+
+    // persist cart data
+    this.persistCartItems();
   }
 
   persistCartItems() {
