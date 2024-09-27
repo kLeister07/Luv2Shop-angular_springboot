38cec33944559fd42ad89dbb0dbb60d12287e90f
Kevin Leister <kevinleister07@gmail.com>
2024-09-27 14:02:14 -0500

    populate users email address at checkout if logged in

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -34,6 +34,8 @@ export class CheckoutComponent implements OnInit {
   shippingAddressStates: State[] = [];
   billingAddressStates: State[] = [];
 
+  storage: Storage = sessionStorage;
+
   constructor(
     private formBuilder: FormBuilder,
     private luv2ShopFormService: Luv2ShopFormService,
@@ -45,6 +47,9 @@ export class CheckoutComponent implements OnInit {
   ngOnInit(): void {
 
 this.reviewCartDetails();
+
+    // read the user's email address from browser storage
+    const theEmail = JSON.parse(this.storage.getItem('userEmail')!);
 
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
@@ -60,7 +65,7 @@ this.reviewCartDetails();
           Luv2ShopValidators.notOnlyWhitespace,
           Validators.pattern('^[a-zA-Z\\s-]+$'),
         ]),
-        email: new FormControl('', [
+        email: new FormControl(theEmail, [
           Validators.required,
           Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
         ]),
