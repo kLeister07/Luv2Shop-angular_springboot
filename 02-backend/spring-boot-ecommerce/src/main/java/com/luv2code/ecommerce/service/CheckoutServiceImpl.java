eab8722fdaacfddf583de7d5ddbd77b87957a062
Kevin Leister <kevinleister07@gmail.com>
2025-01-06 18:07:40 -0600

    add stripe maven dependency, configure stripe api key, create custom paymentInfo DTO, update checkoutService to create a payment intent

--- 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/service/CheckoutServiceImpl.java
@@ -1,24 +1,32 @@
 package com.luv2code.ecommerce.service;
 
 import com.luv2code.ecommerce.dao.CustomerRepository;
+import com.luv2code.ecommerce.dto.PaymentInfo;
 import com.luv2code.ecommerce.dto.Purchase;
 import com.luv2code.ecommerce.dto.PurchaseResponse;
 import com.luv2code.ecommerce.entity.Customer;
 import com.luv2code.ecommerce.entity.Order;
 import com.luv2code.ecommerce.entity.OrderItem;
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class CheckoutServiceImpl implements CheckoutService {
 
 private CustomerRepository customerRepository;
 
-public CheckoutServiceImpl(CustomerRepository customerRepository) {
+public CheckoutServiceImpl(CustomerRepository customerRepository,
+                           @Value("${stripe.key.secret}") String secretKey) {
     this.customerRepository = customerRepository;
+
+//    initialize Stripe API with secret key
+    Stripe.apiKey = secretKey;
 }
 
     @Override
@@ -59,6 +67,20 @@ customerRepository.save(customer);
 //        return a response
         return new PurchaseResponse(orderTrackingNumber);
     }
+
+    @Override
+    public PaymentIntent createPaymentIntent(PaymentInfo paymentInfo) throws StripeException {
+    List<String> paymentMethodTypes = new ArrayList<>();
+    paymentMethodTypes.add("card");
+
+        Map<String, Object> params = new HashMap<>();
+        params.put("amount", paymentInfo.getAmount());
+        params.put("currency", paymentInfo.getCurrency());
+        params.put("payment_method_types", paymentMethodTypes);
+
+        return PaymentIntent.create(params);
+    }
+
     private String generateOrderTrackingNumber() {
 //    generate a random UUID number (UUID version 4)
 //    for details see: hhtp://en.wikipedia.org/wiki/Universally_unique_identifier
