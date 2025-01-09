484479cd9a56f9a6bf4da323be72c2be5c99b769
Kevin Leister <kevinleister07@gmail.com>
2025-01-09 13:28:30 -0600

    update checkoutService to create a paymentIntent, load stripe js library in index.html

--- 03-frontend/angular-ecommerce/src/app/services/checkout.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/checkout.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Purchase } from '../common/purchase';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
+import { PaymentInfo } from '../common/payment-info';
 
 @Injectable({
   providedIn: 'root',
@@ -10,9 +11,15 @@ import { environment } from '../../environments/environment';
 export class CheckoutService {
   private purchaseUrl = environment.luv2shopApiUrl + 'checkout/purchase';
 
+  private paymentIntentUrl = environment.luv2shopApiUrl + 'checkout/payment-intent';
+
   constructor(private hhtpClient: HttpClient) {}
 
   placeOrder(purchase: Purchase): Observable<any> {
     return this.hhtpClient.post<Purchase>(this.purchaseUrl, purchase);
+  }
+
+  createPaymentIntent(paymentInfo: PaymentInfo ): Observable<any> {
+    return this.hhtpClient.post<PaymentInfo>(this.paymentIntentUrl, paymentInfo);
   }
 }
