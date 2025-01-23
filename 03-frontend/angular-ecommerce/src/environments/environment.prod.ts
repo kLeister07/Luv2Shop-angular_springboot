fbfa622b970d041374b3ffb215ad3402a0c39d46
Kevin Leister <kevinleister07@gmail.com>
2025-01-23 14:02:17 -0600

    prep for production

--- 03-frontend/angular-ecommerce/src/environments/environment.prod.ts
+++ 03-frontend/angular-ecommerce/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   luv2shopApiUrl: 'https://localhost:9898/api',
+  stripePublishableKey: "pk_test_51Q7OG8P1gAiCtmXkiFem7AnOGwDj5eWKViU7WecnpPYXXrgMrAe63q39dotbze25Qyvt2WovEiNd2CJeyHMLIFkl001svpYs89"
 };
