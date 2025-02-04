a9b633b1020fda683deb58082922fae67ce345e8
Kevin Leister <kevinleister07@gmail.com>
2025-02-04 15:15:22 -0600

    Update production environment and refresh static files

--- 03-frontend/angular-ecommerce/src/environments/environment.prod.ts
+++ 03-frontend/angular-ecommerce/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  luv2shopApiUrl: 'https://localhost:9898/api',
+  luv2shopApiUrl: 'https://luv2shop-angularspringboot-production.up.railway.app/api',
   stripePublishableKey: "pk_test_51Q7OG8P1gAiCtmXkiFem7AnOGwDj5eWKViU7WecnpPYXXrgMrAe63q39dotbze25Qyvt2WovEiNd2CJeyHMLIFkl001svpYs89"
 };
