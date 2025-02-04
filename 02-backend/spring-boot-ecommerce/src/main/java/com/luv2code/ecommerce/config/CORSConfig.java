827f5d078fcafc831e46ddb70e25a6f19f5b0719
Kevin Leister <kevinleister07@gmail.com>
2025-02-03 21:02:57 -0600

    Added Java-based CORS configuration

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/luv2code/ecommerce/config/CORSConfig.java
@@ -0,0 +1,23 @@
+package com.luv2code.ecommerce.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://luv2shop-angularspringboot-production.up.railway.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
